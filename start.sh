716fc73c92a1eb00e4d644d4aca0e97852491c5f
Julian Mejliker <jmejliker@gmail.com>
2019-09-21 12:10:16 -0300

    Agregadas validaciones en start y stop

--- start.sh
+++ start.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
 
-. ./process.sh
+CORRIENDO=0
+
+exec 6>&1 #Guardo el stdout default
+exec > "stop.log"
+ps -ef | grep ".*process.sh" | grep -v grep   
+
+if [ $? -ne 0 ]; then
+  exec 1>&6 6>&-
+  echo "Se Corre el proceso correctamente"
+  bash ./process.sh
+else
+  exec 1>&6 6>&-
+  echo "No se puede correr el roceso, ya que hay otro corriendo"
+  exit
+fi
+
+
--- stop.sh
+++ stop.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
-ps -ef | grep ".*process.sh" | grep -v grep | awk '{print $2}' | xargs kill
+exec 6>&1 #Guardo el stdout default
+exec > "stop.log"
+ps -ef | grep ".*process.sh" | grep -v grep
+
+if [ $? -eq 0 ]; then
+  ps -ef | grep ".*process.sh" | grep -v grep | awk '{print $2}' | xargs kill
+  exec 1>&6 6>&-
+  echo "El proceso fue finalizado de manera exitosa"
+else
+  exec 1>&6 6>&-
+  echo "No se esta corriendo ningun proceso, no es posible frenarlos"  
+  exit
+fi
