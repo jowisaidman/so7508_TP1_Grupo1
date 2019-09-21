013006310cf213671ef752628831945c844fcaa3
lituarte <lautaro.javier.ituarte@gmail.com>
2019-09-21 19:04:33 -0300

    Agrego logueo con el formateo pedido en preprocess

--- preprocess.sh
+++ preprocess.sh
@@ -1,3 +1,10 @@
+#Parametros:
+#1) Nombre del proceso/archivo desde donde se loguea
+#2) Mensaje
+logInfo() {
+    echo `date` - $USERNAME - $1 - INF - $2
+}
+
 #TODO: Cambiar para que tome los paths configurados.
 path_novedades='Novedades'
 path_aceptados='Aceptados'
@@ -5,6 +12,8 @@ path_rechazados='Rechazados'
 path_procesados='Procesados'
 
 cd ./$path_novedades
+
+logInfo $0 "Comienza el preprocesamiento."
 
 #Lista de todos los archivos del formato Lote_XX.csv
 archivos_procesables=`ls | grep 'Lote_[0-9][0-9]\?\.csv'`
@@ -15,18 +24,19 @@ archivos_no_procesables=`ls | grep -v 'Lote_[0-9][0-9]\?\.csv'`
 #Rechazo los archivos de nombre invalido.
 for i in $archivos_no_procesables
 do
-    echo El archivo $i no tiene un nombre valido.
+    logInfo $0 "El archivo $i no tiene un nombre valido."
     mv $i ../$path_rechazados
 done
 
+#Preproceso los archivos con nombre valido.
 for i in $archivos_procesables
 do
 	#Valido que el archivo no este vacio.
 	if [ -s $i ];
 	then
-        echo El archivo $i no esta vacio.
+        logInfo $0 "El archivo $i no esta vacio."
 	else
-        echo El archivo $i esta vacio.
+        logInfo $0 "El archivo $i esta vacio."
 		mv $i ../$path_rechazados
 		continue
 	fi
@@ -37,11 +47,11 @@ do
 	#Valido que el archivo no haya sido procesado.
 	if test -f $i;
 	then
-		echo El archivo $i ya fue procesado con anterioridad.
+		logInfo $0 "El archivo $i ya fue procesado con anterioridad."
 		mv $i ../$path_rechazados
 		continue
 	else
-		echo El archivo $i no fue procesado.
+		logInfo $0 "El archivo $i no fue procesado."
 	fi
 
 	cd ..
@@ -49,5 +59,7 @@ do
 
 	mv $i ../$path_aceptados
 
-	echo Fin exitoso del preprocesamiento de $i.
+	logInfo $0 "Fin exitoso del preprocesamiento de $i."
 done
+
+logInfo $0 "Fin del preprocesamiento."
