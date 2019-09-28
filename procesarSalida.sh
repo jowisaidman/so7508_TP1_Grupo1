f0f2a275667a26b0c9641149608f85a46e10abff
lituarte <lautarojavier92@hotmail.com>
2019-09-28 20:49:59 -0300

    Agrego procesamiento de las transacciones, falta crear el msjHost cuando el campo esta vacio

--- procesarSalida.sh
+++ procesarSalida.sh
@@ -71,6 +71,51 @@ do
     echo $tipoOperacion,$descripcionOperacion,$totalTrx,$fechaCierreLote,$anio,$fechaHora,$traceNumber,$codigoISO,$retrievalNumber,$msjHost,$parseados >> Cierre_de_Lote.csv
 
     #Procesar las transacciones
+    transacciones=`echo "$contenido" | grep -v '^CI.*'`
+
+    IFS=$'\n' # para que el "for" separe por linea
+    for j in $transacciones
+    do
+        tipoOperacion=`echo "$j" | cut -d ',' -f1`
+        descripcionOperacion=`echo "$j" | cut -d ',' -f2`
+        anio=`echo "$j" | cut -d ',' -f5`
+        fechaHora=`echo "$j" | cut -d ',' -f6`
+        numeroTarjeta=`echo "$j" | cut -d ',' -f7`
+        vencimiento=`echo "$j" | cut -d ',' -f8`
+        importe=`echo "$j" | cut -d ',' -f9`
+        cuotas=`echo "$j" | cut -d ',' -f10`
+        traceNumber=`echo "$j" | cut -d ',' -f11`
+        codigoISO=`echo "$j" | cut -d ',' -f12`
+        retrievalNumber=`echo "$j" | cut -d ',' -f13`
+        ticketNumber=`echo "$j" | cut -d ',' -f14`
+        autorizacion=`echo "$j" | cut -d ',' -f15`
+        idTrx=`echo "$j" | cut -d ',' -f16`
+        trxRelacionada=`echo "$j" | cut -d ',' -f17`
+        msjHost=`echo "$j" | cut -d ',' -f18`
+
+        if [ `echo "$msjHost" | grep '[a-zA-Z]' -c` == 0 ];
+        then
+            echo "No tiene mensaje host"
+            #TODO: Falta crear el msjHost a partir del campo codigoISO
+        else
+            echo "Tiene mensaje host y es $msjHost"
+        fi
+
+        contenidoFechaHora=`echo "$fechaHora" | cut -d '>' -f2`
+        contenidoImporte=`echo "$importe" | cut -d '>' -f2`
+        contenidoAnio=`echo "$anio" | cut -d '>' -f2`
+
+        mes=${contenidoFechaHora:0:2}
+        dia=${contenidoFechaHora:2:2}
+        hora=${contenidoFechaHora:4:2}:${contenidoFechaHora:6:2}:${contenidoFechaHora:8:2}
+        monto=${contenidoImporte:0:10},${contenidoImporte:10:2}
+
+        parseado=$mes,$dia,$hora,$monto
+        original=$tipoOperacion,$descripcionOperacion,$anio,$fechaHora,$numeroTarjeta,$vencimiento,$importe,$cuotas,$traceNumber,$codigoISO,$retrievalNumber,$ticketNumber,$autorizacion,$idTrx,$trxRelacionada,$msjHost
+
+        echo $original,$parseado >> TRX_$contenidoAnio$mes$dia.csv
+
+    done
 
     cd ..
     cd ./$path_aceptados
