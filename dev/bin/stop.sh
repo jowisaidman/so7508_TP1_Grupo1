308ebcb36133b91acb04f242bac9ff3cff7edd53
Julian Mejliker <jmejliker@gmail.com>
2019-09-30 16:03:18 -0300

    Agregados log en stop y el archivo

--- dev/bin/stop.sh
+++ dev/bin/stop.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 source ./validaciones.sh
-
-exec 6>&1 #Guardo el stdout default
-exec >> "stop.log"
-
+source ./helpers.sh
 
 function killProcess() {
   ps -ef | grep ".*$1.*" | grep -v grep | awk '{print $2}' | xargs kill
@@ -13,10 +10,8 @@ isRunning "process.sh"
 
 if [ $? -eq 0 ]; then
     killProcess "process.sh"
-    echo "$(date +'%d/%m/%Y %T') El proceso fue finalizado de manera exitosa"
+    logInfo "Stop" "El proceso fue finalizado de manera exitosa" "../conf/logs/stop.log"
 else
-  echo "$(date +'%d/%m/%Y %T') No se esta corriendo ningun proceso, no es posible frenarlos"  
-  exec 1>&6 6>&-
+  logInfo "Stop" "No se esta corriendo ningun proceso, no es posible frenarlos"   "../conf/logs/stop.log"
   return 1
 fi
-exec 1>&6 6>&-
